5a5b70eeb23d10f68601730703dd5754d3beb971
Sublimeful <Qiangwu2000@gmail.com>
2021-05-19 03:18:10 -0500

    .

--- src/server/server.js
+++ src/server/server.js
@@ -71,9 +71,9 @@ server.on("connection", client => {
       client.emit("sync", {video: currentVideo});
     }
 
-    //if the difference between client time and server time
-    //is greater than threshold milliseconds then sync client with server
-    if(Math.abs(tools.getTime(currentVideo) - clientTime) > threshold) {
+    //if the difference between client time and server time is
+    //greater than threshold milliseconds then sync client with server
+    if(Math.abs(clientTime - tools.getTime(currentVideo)) > threshold) {
       client.emit("sync", {video: currentVideo});
     }
   })
