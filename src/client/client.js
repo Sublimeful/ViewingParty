3eb4b87bc0070c10c457fbd5a69157cfca7850b3
Sublimeful <Qiangwu2000@gmail.com>
2021-06-12 21:37:46 -0500

    .

--- src/client/client.js
+++ src/client/client.js
@@ -268,7 +268,7 @@ function addLeaderControls()
     ss(client).emit('subtitle', stream);
     blobstream.pipe(stream);
 
-    //reset subtitle input value
+    //reset subtitle input's content
     subtitle.value = "";
   })
 
